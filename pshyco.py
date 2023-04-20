ccd444ee96dc340a2ef24d8c0d6cb29889a1a22f
PSHYCO <131322569+Pshyco-rosetar-2@users.noreply.github.com>
2023-04-20 15:49:16 +0600

    Create pshyco.py

---
+++ pshyco.py
@@ -0,0 +1,15 @@
+import os,time,platform
+os.system('clear')
+print('\n\n \033[1;91m[\033[1;92m✔\033[1;91m]\033[1;93m Checking Updates..........\n')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print("\n \033[1;91m[\033[1;92m✔\033[1;91m]\033[1;92m Your Device \033[1;91m64\033[1;92m Bit Supported This Tool Enjoy.....")
+    time.sleep(2)
+    import RM64
+elif bit=='32bit':
+    print("\n \033[1;91m[\033[1;92m✔\033[1;91m]\033[1;92m Your Device \033[1;91m32\033[1;92m Bit Supported this Tool Enjoy.....")
+    time.sleep(2)
+    import RM32
+else:
+    print('\033[1;31m[\033[1;92m×\033[1;91m] No Internet Connection !!!!!')
